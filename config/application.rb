d41391f8634364f145ac937db8774354c297dcd4
andrey <tormozz48@gmail.com>
2012-11-15 22:11:00 +0200

    production config

--- config/application.rb
+++ config/application.rb
@@ -53,9 +53,9 @@ module Daryatoys
     # Enable the asset pipeline
     config.assets.enabled = true
 
-    config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
+    #config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
