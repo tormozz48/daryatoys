7c9dbfc92a92a71ee955bdad5d65e0a8463503d1
andrey <tormozz48@gmail.com>
2012-11-25 17:03:54 +0200

    fix for email sending for contact comment

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -7,7 +7,7 @@ class CommentMailer < ActionMailer::Base
     mail(
         :from => @comment.email,
         :to => @contact.email,
-        :subject => I18n.t('site.email_subject')
+        :subject => I18n.t('site.email_subject', :site => t('site.title'))
     )
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,13 @@ Daryatoys::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'daryatoys.herokuapp.com',
+      :user_name            => 'tormozoid48@gmail.com',
+      :password             => 'tormozoid',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
 end
