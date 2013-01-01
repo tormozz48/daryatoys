9e91e68caf7bbaabb4cc1fb7771782c3f3907bf8
andrey <tormozz48@gmail.com>
2013-01-02 00:03:46 +0200

    huge refactoring with localization re-design and test coverage 01012013

---
+++ app/models/response.rb
@@ -0,0 +1,19 @@
+class Response < ActiveRecord::Base
+  validates :email, :fio, :title, :body, :presence => true
+  validates :email, :length => {:maximum => 100}
+  validates :fio, :length => {:minimum => 5, :maximum => 100}
+  validates :title, :length => {:maximum => 100}
+  validates :body, :length => {:maximum => 500}
+
+  attr_accessible :email, :fio, :title, :body, :humanizer_question_id, :humanizer_answer
+
+  include Humanizer
+  require_human_on :create
+
+  after_create :send_email
+
+  protected
+  def send_email
+    CommentMailer.comment_email(self).deliver
+  end
+end
